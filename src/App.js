a395670fa73cf8821630b25214747f630dfb00b4
unknown <viterr@gmail.com>
2021-06-02 00:23:21 -0300

    Supa test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { supabase } from './supabase'
+import Login from './components/Login'
+import Account from './components/Account'
 
-function App() {
+export default function Home() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      { console.log(session) }
+      {!session ? <Login /> : <Account key={session.user.id} session={session} />}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import { supabase } from '../supabase'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const Signup = () =>{
+    const [loading, setLoading] = useState(false)
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleInputs = (e) =>{
+        setInputs({
+          ...inputs,
+          [e.target.name]:e.target.value
+
+        })
+    }
+  
+    const handleSignUp = async (e) => {
+      e.preventDefault()
+      try {
+        setLoading(true)
+        const { error } = await supabase.auth.signUp(inputs)
+        if (error) throw error
+        alert('Check your email for the login link!')
+      } catch (error) {
+        alert(error.error_description || error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    return (  
+        <div>
+            <h1>Sign Up </h1>
+            <Form onSubmit={handleSignUp}>
+                <Form.Group controlId="formBasicEmail"  >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleInputs} />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="password" onChange={handleInputs} />
+                </Form.Group>                
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default Signup
